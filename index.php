ed8bb0a396824e8b14b03b6528742a298f0cd3a1
Takahiro Masaki <tak.msk2580@gmail.com>
2014-10-05 13:05:27 +0900

    Add header,main,footer

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Test BBS</title>
+    <title>Snowboarding Style</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,10 +19,15 @@
 
   </head>
   <body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	
 	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class="navbar-header">
 			<!-- Site name(Logo) -->
-			<a class="navbar-brand" href="#">Test BBS</a>
+			<a class="navbar-brand" href="#">Snowboard Style</a>
 			<!-- Setting toggle -->
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-content">
 				<span class="icon-bar"></span>
@@ -32,9 +38,9 @@
 		<div id="nav-content" class="collapse navbar-collapse">
 			<!-- Link list/ Menu list-->
 			<ul class="nav navbar-nav">
-				<li><a href="">Link1</a></li>
-				<li><a href="">Link2</a></li>
-				<li><a href="">Link3</a></li>
+				<li><a href="#">Link1</a></li>
+				<li><a href="#">Link2</a></li>
+				<li><a href="#">Link3</a></li>
 				<!-- Dropdown list -->
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Drop<b class="caret"></b></a>
@@ -58,10 +64,60 @@
 		</div>
 	</nav>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+	<header class="jumbotron">
+		<div class="container">
+			<h1>Snowboard Style</h1>
+				<p>This is a test coding for DMTC.</p>
+				<p><a class="btn btn-lg heading-btn" role="button">Learn more &raquo;</a></p>
+		</div>
+	</header>
+
+	<!-- .container main-content -->
+	<div class="container main-content">
+		<div class="row">
+			<div class="col-md-9 content-area">
+				<h2>Title</h2>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+				<p>Consectetur nobis voluptates repudiandae pariatur soluta dignissimos expedita. Ipsum eius officia laborum voluptate voluptate doloremque quod dolor omnis. Dolore libero ratione itaque excepturi quisquam quos! Ab laboriosam consequatur voluptatum aliquam!</p>
+			</div><!-- /.col-md-9 content-area -->
+			<div class="col-md-3 sidebar">
+				<aside>
+					<h4>Link to category</h4>
+					<ul class="list-unstyled">
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+					</ul>
+				</aside>
+
+				<aside>
+					<h4></h4>
+					<ul class="list-unstyled">
+						<li><a href="#">Title of article</a></li>
+						<li><a href="#">Title of article</a></li>
+						<li><a href="#">Title of article</a></li>
+						<li><a href="#">Title of article</a></li>
+						<li><a href="#">Title of article</a></li>
+					</ul>
+				</aside>
+			</div><!-- /.col-md-3 sidebar -->
+		</div>
+	</div><!-- /.container main-content -->
+
+	<footer class="container-fluid">
+		<small>Copyright (C) 2014 Snowboard Style.</small>
+	</footer>
+
   </body>
 </html>
 
