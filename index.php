ed7917aca04fbb0bf0cd637decc29febc22ce0d9
Takahiro Masaki <tak.msk2580@gmail.com>
2014-10-05 16:32:49 +0900

    Add form design

--- index.php
+++ index.php
@@ -3,6 +3,38 @@
 ?>
 <!-- .container main-content -->
 <div class="container main-content">
+	<div class="row form">
+		<form action="validate.php" class="form-horizontal" method="post">
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-2">
+					<h2>Comment</h2>
+				</div>
+			</div>
+			<p class="alert">
+				<!-- validation -->
+			</p>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-7">
+					<input type="text" name="name" value="" class="form-control" placeholder="Name">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-7">
+					<input class="form-control" type="text" value="" placeholder="E-mail">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-8">
+					<textarea class="form-control" name="comment" value="" rows="5" placeholder="Comment"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-2">
+					<button type="submit" class="btn btn-default">Send</button>
+				</div>
+			</div>
+		</form>
+	</div>
 	<div class="row">
 		<div class="col-md-9 content-area">
 			<h2>Title</h2>
